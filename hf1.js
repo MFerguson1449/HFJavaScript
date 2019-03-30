98c69022d158bed91d9823f9be53cd4bc2c79dd4
MFerguson1449 <MFerguson1449@gmail.com>
2019-03-30 00:01:35 -0400

    initial commit

---
+++ hf1.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Just a Generic Page</title>
+        <script>
+            var name = 'joe';
+            var i = 0;
+
+            while (i < 2) {
+                document.write("Happy Birthday to you!<br>");
+                i = i + 1;
+            }
+            document.write("Happy Birthday dear " + name + ",<br>");
+            document.write("Happy Birthday to you!<br>");
+        </script>
+    </head>
+    
+    <body>
+        <h1>Just a generic heading</h1>
+        <p>Not a lot to read about here. I'm just an obligatory paragraph living in an example
+             i na JavaScript book. I'm looking for something to make my life more exciting.</p>
+    </body>
+</html>
